681ca830c77bd1524ad88baaf8b026b776df2574
Victor Carvalho <victorcarvalhosp@gmail.com>
2019-11-10 11:19:08 -0300

    Updates

---
+++ static/js/5.090a37b7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-hook-form-issue"]=this["webpackJsonpreact-hook-form-issue"]||[]).push([[5],{182:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(26),i=(o(5),function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.g)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then((function(){Object(e.m)((function(){return r.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.090a37b7.chunk.js.map
